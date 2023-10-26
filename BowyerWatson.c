6e3e89dbb2c822ac0d7c64c554841c7fa0814942
zibbe77 <theo@zibll.se>
2023-10-26 15:22:39 +0200

    Started on the algritm loop

--- BowyerWatson.c
+++ BowyerWatson.c
@@ -27,13 +27,11 @@ typedef struct TriangleData
 
 } TriangleData;
 
-typedef struct triangleInfo
+typedef struct CircomCenterInfo
 {
-    pair_o center;
-    pair_o firstPoint;
-    pair_o secendPoint;
-    pair_o tiridPoint;
-} triangleInfo;
+    int cordent;
+    float radius;
+} CircomCenterInfo;
 
 // adding super triangle
 
@@ -42,34 +40,64 @@ PointsToCumpute pointsToCumpute = {.x = 10,
 
 pair_o ConvertIntCordToPair(int data, int width)
 {
-    printf("data %d \n", data);
+    // printf("data %d \n", data);
 
     pair_o utput;
     utput.c1 = data % width;
     utput.c2 = floor(data / width);
 
-    printf("x %f y %f \n ", utput.c1, utput.c2);
+    // printf("x %f y %f \n ", utput.c1, utput.c2);
     return utput;
 }
 
-triangleInfo CallCircumCenter(int index, GraphVertex *vertex, int width)
+float FindDistans(pair_o point, pair_o center)
+{
+    Vector2 pointV = {.x = point.c1,
+                      .y = point.c2};
+    Vector2 centerV = {.x = center.c1,
+                       .y = center.c2};
+
+    return Vector2Distance(pointV, centerV);
+}
+
+CircomCenterInfo CallCircumCenter(int index, TriangleData triangledata, int width)
 {
     // get all points in a triangle
-    pair_o P = ConvertIntCordToPair(ReaddataOne(vertex, index, 0), width);
-    pair_o Q = ConvertIntCordToPair(ReaddataOne(vertex, index, 1), width);
-    pair_o R = ConvertIntCordToPair(ReaddataOne(vertex, index, 2), width);
+    pair_o P = ConvertIntCordToPair(triangledata.p1.cordent, width);
+    pair_o Q = ConvertIntCordToPair(triangledata.p2.cordent, width);
+    pair_o R = ConvertIntCordToPair(triangledata.p3.cordent, width);
 
-    // input points to pair_o
-    // pair_o P = {.c1 = 11, .c2 = 0};
-    // pair_o Q = {.c1 = 0, .c2 = 0};
-    // pair_o R = {.c1 = 0, .c2 = 8};
-    triangleInfo interfaceUtput;
-    interfaceUtput.center = Interface(P, Q, R);
-    interfaceUtput.firstPoint = P;
-    interfaceUtput.secendPoint = Q;
-    interfaceUtput.tiridPoint = R;
+    pair_o TempPair = Interface(P, Q, R);
+    CircomCenterInfo circomCenterInfo;
+    circomCenterInfo.cordent = TempPair.c2 * width + TempPair.c1;
+    circomCenterInfo.radius = FindDistans(P, TempPair);
 
-    return interfaceUtput;
+    return circomCenterInfo;
+}
+
+Vector2 ConvertIntCordToVector2(int data, int width)
+{
+    Vector2 utput;
+    utput.x = data % width;
+    utput.y = floor(data / width);
+    return utput;
+}
+
+bool IsRadiusLess(float distance, int point, int newPoint, int width)
+{
+    Vector2 pointV = ConvertIntCordToVector2(point, width);
+    Vector2 newPointV = ConvertIntCordToVector2(newPoint, width);
+
+    float distancePtoNP = Vector2Distance(pointV, newPointV);
+
+    if (distance > distancePtoNP)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 void DebugDrawTriangle(GraphVertex *vertex, int graphSize, int width)
@@ -123,13 +151,70 @@ void BowyerWatson(int width, int *pointList, int listSize)
     TriangleData triangleDatatemp = {.p1.cordent = 0, .p1.index = 0, .p2.cordent = 4095, .p2.index = 1, .p3.cordent = 63, .p3.index = 2};
     triangleData[0] = triangleDatatemp;
 
-    triangleInfo callCircumCenterUtput = CallCircumCenter(0, vertex, width);
+    // active triangles
+    int usedTriangles;
+
+    // for the super triangle
+    usedTriangles++;
+
+    // invalide triangles index
+    int invalideTriangles[20];
+
+    for (int i = 0; i < 20; i++)
+    {
+        invalideTriangles[0] = 0;
+    }
 
     // insert point
-    AddToHeadSlot(vertex, 3, pointList[0]);
-    AddAdjacentNode(vertex, 3, 0);
-    AddAdjacentNode(vertex, 3, 4095);
-    AddAdjacentNode(vertex, 3, 63);
+    // AddToHeadSlot(vertex, 3, pointList[0]);
+    // AddAdjacentNode(vertex, 3, 0);
+    // AddAdjacentNode(vertex, 3, 4095);
+    // AddAdjacentNode(vertex, 3, 63);
+
+    // ändra 1 sen till list size
+    for (int i = 0; i < 1; i++)
+    {
+        int prosesingTriangleIndexList[20];
+        int prosesingTriNum = 0;
+        for (int j = 0; j < 20; j++)
+        {
+            prosesingTriangleIndexList[j] = 0;
+        }
+
+        // insert point
+        AddToHeadSlot(vertex, i + 3, pointList[i]);
+
+        // findes all circum centers and exlueds non ative ones and invalid
+        for (int j = 0; j < usedTriangles; j++)
+        {
+            if (invalideTriangles[j] == 1)
+            {
+                break;
+            }
+
+            CircomCenterInfo circumCenterUtPut = CallCircumCenter(0, triangleData[j], width);
+            printf("C %d R %f \n", circumCenterUtPut.cordent, circumCenterUtPut.radius);
+
+            // look if point is less then raidus add it to a list
+            if (IsRadiusLess(circumCenterUtPut.radius, triangleData[j].p1.cordent, pointList[i], width))
+            {
+                prosesingTriangleIndexList[prosesingTriNum] = j;
+                prosesingTriNum++;
+            }
+        }
+
+        // Remove conestions
+
+        // Add conestions
+        for (int j = 0; j < prosesingTriNum; j++)
+        {
+            AddAdjacentNode(vertex, i + 3, triangleData[i].p1.cordent);
+            AddAdjacentNode(vertex, i + 3, triangleData[i].p2.cordent);
+            AddAdjacentNode(vertex, i + 3, triangleData[i].p3.cordent);
+        }
+
+        // Look for invalides
+    }
 
     // debug
     DebugDrawTriangle(vertex, graphSize, width);
@@ -147,6 +232,7 @@ int main(int argc, char const *argv[])
     pointList[0] = 655;
 
     BowyerWatson(64, pointList, 5);
+    DrawCircleLines(31 * 5 + 100, 31 * 5 + 100, 44.547726 * 5, WHITE);
     EndDrawing();
     while (!WindowShouldClose())
     {
